978e8f778a64f867dc7fb4abff2c0de2117dd5af
kshino1208 <kshino1208@gmail.com>
2021-01-23 20:13:50 +0900

    ログ削除

--- app/src/main/java/jp/techacademy/kazuki/shinoda/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kazuki/shinoda/calcapp/MainActivity.kt
@@ -3,7 +3,6 @@ package jp.techacademy.kazuki.shinoda.calcapp
 import com.google.android.material.snackbar.Snackbar
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,7 +30,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                  val value2 = editText2.text.toString().toDouble()
                  val intent = Intent(this, Result::class.java)
 
-                Log.d("test", "エラー１")
                 when(v.id){
                     R.id.button1 -> intent.putExtra("RESULT", value1 + value2)
                     R.id.button2 -> intent.putExtra("RESULT", value1 - value2)
@@ -39,36 +37,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     R.id.button4 -> intent.putExtra("RESULT", value1 / value2)
                 }
                 startActivity(intent)
-                Log.d("test", "エラー２")
 
             } catch (e: Exception){
-                Log.d("test", "エラー３")
 
                 val snackbar = Snackbar.make(root_layout,"何か数値を入力してください", Snackbar.LENGTH_LONG)
                 snackbar.show()
 
             }
 
-
-
-//            if (value1 != null || value2 != null) {
-//                Log.d("ANDROID", "エラー１")
-//                when(v.id){
-//                    R.id.button1 -> intent.putExtra("RESULT", value1 + value2)
-//                    R.id.button2 -> intent.putExtra("RESULT", value1 - value2)
-//                    R.id.button3 -> intent.putExtra("RESULT", value1 * value2)
-//                    R.id.button4 -> intent.putExtra("RESULT", value1 / value2)
-//                }
-//                startActivity(intent)
-//                Log.d("ANDROID", "エラー２")
-//
-//            } else {
-//                Log.d("ANDROID", "エラー３")
-//
-//                val snackbar = Snackbar.make(root_layout,"メッセージ", Snackbar.LENGTH_LONG)
-//                snackbar.show()
-//
-//            }
 
 
         }
